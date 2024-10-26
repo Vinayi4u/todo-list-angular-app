09a5f99ee5b6fc7dfa8eb95050a6a95b8794686f
Vinayi4u <61754506+Vinayi4u@users.noreply.github.com>
2024-10-26 19:28:33 -0400

    use input decorator to pass values from app component to user component

--- todo-app/src/app/user/user.component.ts
+++ todo-app/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,14 +9,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get userImage() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+  onSelectUser() {}
 }
