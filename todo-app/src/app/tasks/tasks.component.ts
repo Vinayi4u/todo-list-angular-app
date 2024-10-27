f77d4bf039b1582e9dcad84d3d85174234e6ad4f
Vinayi4u <61754506+Vinayi4u@users.noreply.github.com>
2024-10-27 09:37:38 -0400

    action add new task

--- todo-app/src/app/tasks/tasks.component.ts
+++ todo-app/src/app/tasks/tasks.component.ts
@@ -1,20 +1,30 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from './dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isTaskAdding = false;
   tasks = dummyTasks;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(taskId: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isTaskAdding = true;
   }
 }
